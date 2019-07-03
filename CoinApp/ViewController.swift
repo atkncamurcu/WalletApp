219f82259dcbca964cabdeeb7e12dda397c9a324
Atakan Camurcu <atkncamurcu@hotmail.com>
2019-07-03 15:34:30 +0300

    Initial Commit

---
+++ CoinApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoinApp
+//
+//  Created by ATAKAN on 3.07.2019.
+//  Copyright © 2019 Xicy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
